9207836556ca293a3b78275daac31b8bc147f107
Maximilian Seidler <paideia_dilemma@losfuzzys.net>
2025-02-05 18:16:01 +0100

    core: check if on_(un)lock_cmd is empty

--- src/core/Hypridle.cpp
+++ src/core/Hypridle.cpp
@@ -351,8 +351,10 @@ void CHypridle::onLocked() {
     m_isLocked = true;
 
     static const auto LOCKCMD = g_pConfigManager->getValue<Hyprlang::STRING>("general:on_lock_cmd");
-    if (*LOCKCMD)
-        spawn(*LOCKCMD);
+    if (*LOCKCMD) {
+        if (const std::string LOCKCMDSTR{*LOCKCMD}; !LOCKCMDSTR.empty())
+            spawn(LOCKCMDSTR);
+    }
 
     if (m_inhibitSleepBehavior == SLEEP_INHIBIT_LOCK_NOTIFY)
         uninhibitSleep();
@@ -366,8 +368,10 @@ void CHypridle::onUnlocked() {
         inhibitSleep();
 
     static const auto UNLOCKCMD = g_pConfigManager->getValue<Hyprlang::STRING>("general:on_unlock_cmd");
-    if (*UNLOCKCMD)
-        spawn(*UNLOCKCMD);
+    if (*UNLOCKCMD) {
+        if (const std::string UNLOCKCMDSTR{*UNLOCKCMD}; !UNLOCKCMDSTR.empty())
+            spawn(UNLOCKCMDSTR);
+    }
 }
 
 CHypridle::SDbusInhibitCookie CHypridle::getDbusInhibitCookie(uint32_t cookie) {
